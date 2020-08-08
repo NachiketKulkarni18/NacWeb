1e309d8ef218686675b2dbb2b4cecc85ad5903c6
nachiket <nachiketkulkarni18052000@gmail.com>
2020-08-08 15:40:16 +0530

    Updates

---
+++ precache-manifest.2dddb1cde1d62ef1dd69c6990818bf0e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3140b1618bcd16e10fd547debe04a767",
+    "url": "/nacweb/index.html"
+  },
+  {
+    "revision": "6b57815881951645973b",
+    "url": "/nacweb/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "05f5e1f14dbf40805b69",
+    "url": "/nacweb/static/css/main.4990ece5.chunk.css"
+  },
+  {
+    "revision": "6b57815881951645973b",
+    "url": "/nacweb/static/js/2.82b5c6f4.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/nacweb/static/js/2.82b5c6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f5e1f14dbf40805b69",
+    "url": "/nacweb/static/js/main.a1a666bc.chunk.js"
+  },
+  {
+    "revision": "ca7962daf632ac27f586",
+    "url": "/nacweb/static/js/runtime-main.c7459b85.js"
+  },
+  {
+    "revision": "ac85acb02b53e361283c55e31416cc0f",
+    "url": "/nacweb/static/media/aliceblue.ac85acb0.png"
+  },
+  {
+    "revision": "36028347dacd3ea2b9bb47df42a1bcf2",
+    "url": "/nacweb/static/media/datasvg.36028347.png"
+  },
+  {
+    "revision": "10d5a07a0f6e6b7b095c70e8e9cc02e3",
+    "url": "/nacweb/static/media/one.jpg.10d5a07a.jpeg"
+  },
+  {
+    "revision": "5b7001faf52c535e16f1eaa000a20d36",
+    "url": "/nacweb/static/media/two.jpg.5b7001fa.jpeg"
+  },
+  {
+    "revision": "62af603ac69218ef74170d9df8bfd850",
+    "url": "/nacweb/static/media/zerodha.62af603a.png"
+  }
+]);
